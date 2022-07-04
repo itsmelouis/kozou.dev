67f830e0b0bd94fc8d27579c10e061f73cc50713
Kozou <louisfloquey@gmail.com>
2022-07-04 21:54:23 +0200

    feat(Components): add Home components

---
+++ components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div
+		class="flex flex-row justify-center w-full h-full min-h-screen text-black dark:text-white bg-white dark:bg-[#12181d]"
+	>
+		<Header />
+		<div class="mt-24 dark:bg-[#12181d]">
+			<h1 class="mb-6 text-3xl font-extrabold">Home</h1>
+		</div>
+	</div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+
+export default {
+	name: "HomePage",
+	components: {
+		Header,
+	},
+};
+</script>
