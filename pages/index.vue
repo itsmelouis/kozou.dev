a351c458a5a96e085073d7ff1c72a1fb85c5edfc
Kozou <louisfloquey@gmail.com>
2022-07-04 21:52:57 +0200

    feat(Pages): add Home and Contact pages

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<div
+		class="flex flex-row justify-center w-full h-full min-h-screen text-black dark:text-white bg-white dark:bg-gray-900"
+		:class="dark ? 'dark' : 'light'"
+	>
+		<HomePage />
+	</div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import HomePage from "../components/Home.vue";
+
+export default {
+	name: "IndexPage",
+	components: { HomePage },
+	head() {
+		return {
+			title: "Home | kozou.dev",
+		};
+	},
+	computed: {
+		...mapGetters(["dark"]),
+	},
+};
+</script>
