2def62e230609358b4a147fb0c3ac07c2408dc4f
Kozou <louisfloquey@gmail.com>
2022-07-04 21:55:04 +0200

    feat(Layouts): add default layout

---
+++ layouts/DefaultLayout.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="transition" :class="dark ? 'dark' : 'light'">
+		<Nuxt />
+	</div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+	computed: {
+		...mapGetters(["dark"]),
+	},
+};
+</script>
