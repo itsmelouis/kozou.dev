d236b91857b6d1db848d172d9c99a388042d20c7
Kozou <louisfloquey@gmail.com>
2022-07-04 21:52:21 +0200

    init commit

---
+++ nuxt.config.js
@@ -0,0 +1,71 @@
+export default {
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: "kozou.dev",
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ hid: "description", name: "description", content: "" },
+			{ name: "format-detection", content: "telephone=no" },
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+	},
+
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
+
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
+
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
+
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/eslint
+		"@nuxtjs/eslint-module",
+		// https://go.nuxtjs.dev/tailwindcss
+		"@nuxtjs/tailwindcss",
+		"@nuxtjs/color-mode",
+	],
+
+	tailwindcss: {
+		jit: true,
+		exposeConfig: true,
+	},
+	colorMode: {
+		classSuffix: "",
+	},
+
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/axios
+		"@nuxtjs/axios",
+		// https://go.nuxtjs.dev/pwa
+		"@nuxtjs/pwa",
+	],
+
+	loading: {
+		color: "blue",
+		height: "10px",
+		duration: 1000,
+		rtl: true,
+		continuous: true,
+	},
+
+	// Axios module configuration: https://go.nuxtjs.dev/config-axios
+	axios: {
+		// Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+		baseURL: "/",
+	},
+
+	// PWA module configuration: https://go.nuxtjs.dev/pwa
+	pwa: {
+		manifest: {
+			lang: "en",
+		},
+	},
+
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
+};
