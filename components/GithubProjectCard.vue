8a64a5036b2cc3765c27834beff81a29924daffb
Kozou <louisfloquey@gmail.com>
2022-07-08 00:39:59 +0200

    feat(Components): add GithubProjectCard component

---
+++ components/GithubProjectCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img
+      src="https://github-readme-stats.vercel.app/api/pin?username=anuraghazra&repo=github-readme-stats&title_color=fff&icon_color=f9f9f9&text_color=9f9f9f&bg_color=151515"
+      alt="Project"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GithubProjectCard'
+}
+</script>
+
+<style></style>
