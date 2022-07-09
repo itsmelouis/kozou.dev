4b3506ac31d87d17a9cc5b05cc2f7dcb0caa6a1b
Kozou <louisfloquey@gmail.com>
2022-07-10 00:34:50 +0200

    refactor(Home component): remove useless code and improve stuff

--- components/Home.vue
+++ components/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <div
-    class="text-black dark:text-white flex flex-row justify-center w-full h-full bg-gradient-to-bl from-white to-[#fff] dark:from-black dark:to-[#0d131f] min-h-screen"
-  >
+    class="transition-all duration-200 text-black dark:text-white flex flex-row justify-center w-full h-full bg-gradient-to-bl from-white to-[#fff] dark:from-black dark:to-[#0d131f] min-h-screen">
     <Header />
-    <div class="mt-32 w-[80%] md:w-[45rem] transition ease-in-out delay-150">
+    <div class="mt-32 w-[80%] md:w-[45rem]">
       <div>
-        <h1 class="mt-18 font-bold text-4xl md:text-5xl mb-4">Hiya, I'm Kozou!</h1>
+        <div class="flex flex-row items-center mt-18 mb-4">
+          <h1 class="font-bold text-4xl md:text-5xl">Hiya, I'm Kozou!</h1>
+          <p class="font-bold text-4xl md:text-5xl">👋</p>
+        </div>
         <p class="text-gray-800 dark:text-gray-300 leading-relaxed tracking-wide mb-12">
           I'm a french student in IT. I'm currently studying in a BTS SIO in order to proudly become
           a full-stack web developer. I've been studied for 1 year now and I still have so much to
@@ -13,19 +15,22 @@
         </p>
       </div>
       <div>
-        <h2 class="font-medium text-3xl mb-4">Technologies</h2>
-        <p class="text-gray-800 dark:text-gray-300 leading-relaxed tracking-wide">
-          I barely know few languages but fair enough to build a simple and friendly web site.
+        <h2 class="font-medium text-3xl mb-4">Technologies 💻</h2>
+        <p class="text-gray-800 dark:text-gray-300 leading-relaxed tracking-wide mb-12">
+          I barely know few languages and tools but fair enough to build a simple and accessible web
+          site. I mainly use frameworks and tools related to Javascript language.
         </p>
       </div>
-      <GitHubCardVue />
-      <GithubProjectCardVue />
-      <CardPositionVue
-        link="https://premid.app/"
-        title="PreMiD"
-        description="PreMiD is a simple, configurable utility that allows you to show what you're doing on the web in your Discord now playing status."
-        position="Technical Support - Moderator"
-      />
+      <div class="mb-12">
+        <h2 class="font-medium text-3xl mb-4">My Projects 🌐</h2>
+        <GithubProjectCardVue />
+      </div>
+      <div>
+        <h2 class="font-medium text-3xl mb-4">Jobs 🧑‍🔧</h2>
+        <CardPositionVue link="https://premid.app/" title="PreMiD"
+          description="PreMiD is a simple, configurable utility that allows you to show what you're doing on the web in your Discord now playing status."
+          position="Technical Support - Moderator" />
+      </div>
       <FooterPageVue />
     </div>
   </div>
@@ -33,7 +38,6 @@
 
 <script>
 import Header from './Header.vue'
-import GitHubCardVue from './GitHubCard.vue'
 import GithubProjectCardVue from './GithubProjectCard.vue'
 import CardPositionVue from './CardPosition.vue'
 import FooterPageVue from './FooterPage.vue'
@@ -42,7 +46,6 @@ export default {
   name: 'HomePage',
   components: {
     Header,
-    GitHubCardVue,
     GithubProjectCardVue,
     CardPositionVue,
     FooterPageVue
