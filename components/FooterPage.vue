de992d21919de20d3d747012bd0f627c795cca5d
Kozou <louisfloquey@gmail.com>
2022-07-10 00:31:56 +0200

    feat(Footer component): add credits and infos to footer

--- components/FooterPage.vue
+++ components/FooterPage.vue
@@ -1,9 +1,14 @@
 <template>
-  <div
-    class="w-full flex flex-col items-start border-t-2 border-slate-800 px-4 py-8 dark:border-opacity-50 mt-8"
-  >
+  <div class="w-full flex flex-col items-start border-t-2 border-slate-800 px-4 py-8 dark:border-opacity-50 mt-8">
     <h1 class="text-black dark:text-white/50 text-2xl font-semibold">Kozou</h1>
-    <h2 class="text-black/60 dark:text-white/30 text-base">Web developper • {{ year }}</h2>
+    <div class="flex flex-row justify-between items-center gap-5 w-full">
+      <h2 class="text-black/60 dark:text-white/30 text-base">Web developper • {{ year }}</h2>
+      <h2 class="text-black/60 dark:text-white/30 text-base">
+        Design credits to
+        <a href="https://github.com/cnrad"
+          class="border-b pb-[1px] border-black/60 dark:border-white/30 transition duration-300 hover:text-black hover:border-black dark:hover:text-white dark:hover:border-white">Cnrad</a>
+      </h2>
+    </div>
   </div>
 </template>
 
