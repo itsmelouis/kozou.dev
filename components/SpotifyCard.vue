07686fc0f09429fb736198c3ee0e4de6a9a025f1
Kozou <louisfloquey@gmail.com>
2022-07-18 02:34:21 +0200

    fix(SpotifyCard): fixed when component still loading even though there's no data

--- components/SpotifyCard.vue
+++ components/SpotifyCard.vue
@@ -1,29 +1,27 @@
 <template>
-  <transition>
-    <div v-if="spotify"
-      class="fixed left-6 -bottom-0 w-[20rem] h-[7rem] hidden lg:flex flex-col items-start justify-start">
-      <h1 class="text-black dark:text-gray-100 font-semibold text-base mb-2 flex items-center justify-center">
-        Listening to Spotify
-        <span class="ml-2 w-2 h-2">
-          <span class="absolute w-2 h-2 bg-red-600 rounded-full animate-ping" />
-          <span class="absolute w-2 h-2 bg-red-600 rounded-full" />
-        </span>
-      </h1>
+  <div v-if="spotify"
+    class="fixed left-6 -bottom-0 w-[20rem] h-[7rem] hidden lg:flex flex-col items-start justify-start">
+    <h1 class="text-black dark:text-gray-100 font-semibold text-base mb-2 flex items-center justify-center">
+      Listening to Spotify
+      <span class="ml-2 w-2 h-2">
+        <span class="absolute w-2 h-2 bg-red-600 rounded-full animate-ping" />
+        <span class="absolute w-2 h-2 bg-red-600 rounded-full" />
+      </span>
+    </h1>
 
-      <div class="w-full h-[6rem] flex flex-row items-center justify-start">
-        <img :src="albumURL" class="w-[4.5rem] h-[4.5rem] rounded-md mr-4 pointer-events-none" :alt="album" />
-        <div class="w-56 h-full flex flex-col items-start justify-center">
-          <a :href="songURL" target="_blank" rel="noreferrer"
-            class="w-full font-medium text-gray-900 dark:text-[#e1eafd] hover:underline truncate">
-            {{ song }}
-          </a>
-          <p class="w-full text-gray-600 dark:text-[#cad2e0] font-normal text-sm truncate">
-            {{ artist }}
-          </p>
-        </div>
+    <div class="w-full h-[6rem] flex flex-row items-center justify-start">
+      <img :src="albumURL" class="w-[4.5rem] h-[4.5rem] rounded-md mr-4 pointer-events-none" :alt="album" />
+      <div class="w-56 h-full flex flex-col items-start justify-center">
+        <a :href="songURL" target="_blank" rel="noreferrer"
+          class="w-full font-medium text-gray-900 dark:text-[#e1eafd] hover:underline truncate">
+          {{ song }}
+        </a>
+        <p class="w-full text-gray-600 dark:text-[#cad2e0] font-normal text-sm truncate">
+          {{ artist }}
+        </p>
       </div>
     </div>
-  </transition>
+  </div>
 </template>
 
 <script>
@@ -35,7 +33,7 @@ export default {
     return {
       userId: "379362415428632576",
       lanyardSocket: null,
-      spotify: {},
+      spotify: null,
       song: null,
       artist: null,
       albumURL: null,
@@ -68,15 +66,3 @@ export default {
   },
 }
 </script>
-
-<style>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 2s ease-in-out;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-</style>
